8d82357257b64b4097869153ae421ce01eccb0e2
Psy-Rat <micekitty@ya.ru>
2021-05-11 01:01:22 +0300

    Вынес базу в корень приложения

--- app/app/config/config.py
+++ app/app/config/config.py
@@ -4,7 +4,7 @@ import os
 class Configuration(object):
     DEBUG = True
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/data.db' % APPLICATION_DIR
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/../data.db' % APPLICATION_DIR
     SITE_WIDTH = 1140
     ADMIN_PASSWORD = 'secret'
     SECRET_KEY = 'shhh, secret!'
